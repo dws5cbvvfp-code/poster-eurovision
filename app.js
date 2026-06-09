da6a3c7f745d8c5bcb0117d1086880cd0dc6d22c
dws5cbvvfp-code <dws5cbvvfp@privaterelay.appleid.com>
2026-06-09 15:12:15 +0200

    fix: restore missing comment in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ paper.install(window);
 window.onload = function() {
     paper.setup('canvas');
     project.importSVG('poster.svg', function(item) {
+        // Estrai il cuore e inizializza
         const cuore = project.getItem({ id: 'cuore' });
         cuore.remove();
         const symbol = new SymbolDefinition(cuore);
