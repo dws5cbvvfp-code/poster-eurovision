7da892906825fc15a3f9e3d09743294e1fae3917
dws5cbvvfp-code <dws5cbvvfp@privaterelay.appleid.com>
2026-06-09 15:11:16 +0200

    feat: initial project setup

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ paper.install(window);
 window.onload = function() {
     paper.setup('canvas');
     project.importSVG('poster.svg', function(item) {
-        // Estrai il cuore e inizializza
         const cuore = project.getItem({ id: 'cuore' });
         cuore.remove();
         const symbol = new SymbolDefinition(cuore);
